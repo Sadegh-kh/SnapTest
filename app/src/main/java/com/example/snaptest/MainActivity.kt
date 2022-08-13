03378f0423577e42b02cbd120564e441624af3ae
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-13 12:03:25 +0430

    init testing image slider

--- app/src/main/java/com/example/snaptest/MainActivity.kt
+++ app/src/main/java/com/example/snaptest/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.snaptest
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.snaptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,16 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val imagList=ArrayList<SlideModel>()
+        imagList.add(SlideModel(R.drawable.img_slider1))
+        imagList.add(SlideModel(R.drawable.img_slider2))
+        imagList.add(SlideModel(R.drawable.img_slider3))
+        imagList.add(SlideModel(R.drawable.img_slider4))
+        imagList.add(SlideModel(R.drawable.img_slider5))
+        imagList.add(SlideModel(R.drawable.img_slider5))
+        imagList.add(SlideModel(R.drawable.img_slider6))
+
+        binding.slider.imageSlider.setImageList(imagList,ScaleTypes.FIT)
     }
 }
