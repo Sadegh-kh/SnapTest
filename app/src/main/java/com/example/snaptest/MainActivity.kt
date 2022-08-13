5f17ddc4d6987f505a57d57e823f24bc609a152d
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-13 14:23:35 +0430

    added image slider on fragment home

--- app/src/main/java/com/example/snaptest/MainActivity.kt
+++ app/src/main/java/com/example/snaptest/MainActivity.kt
@@ -14,17 +14,17 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        val imagList=ArrayList<Int>()
-        imagList.add(R.drawable.img_slider1)
-        imagList.add(R.drawable.img_slider2)
-        imagList.add(R.drawable.img_slider3)
-        imagList.add(R.drawable.img_slider4)
-        imagList.add(R.drawable.img_slider5)
-        imagList.add(R.drawable.img_slider6)
-
-//        binding.slider.imageSlider.setImageList(imagList,ScaleTypes.FIT)
-
-        binding.viewPagerMain.adapter=ViewPagerAdapter(this,imagList)
+//
+//        val imagList=ArrayList<Int>()
+//        imagList.add(R.drawable.img_slider1)
+//        imagList.add(R.drawable.img_slider2)
+//        imagList.add(R.drawable.img_slider3)
+//        imagList.add(R.drawable.img_slider4)
+//        imagList.add(R.drawable.img_slider5)
+//        imagList.add(R.drawable.img_slider6)
+//
+////        binding.slider.imageSlider.setImageList(imagList,ScaleTypes.FIT)
+//
+//        binding.viewPagerMain.adapter=ViewPagerAdapter(this,imagList)
     }
 }
